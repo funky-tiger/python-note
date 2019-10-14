9a36ef2f6f9a507cf75bea1fe57234cfccb89133
tiger <764427188@qq.com>
2019-10-14 17:53:54 +0800

    add:function

---
+++ function/a1.py
@@ -0,0 +1,23 @@
+
+
+# def add(a, b):
+#     return a+b
+
+# print(add(1, 2))
+
+# def printValue():
+#     print('请输入:')
+#     v = input()
+#     print(v)
+
+# printValue()
+
+
+def getValue(a, b):
+    val1 = a*2
+    val2 = b + 26
+    return val1, val2
+
+
+valA, valB = getValue(1, 2)
+print(valA, valB)
